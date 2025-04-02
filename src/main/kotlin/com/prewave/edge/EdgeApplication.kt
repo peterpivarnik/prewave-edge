ac658eef87cc4e1a61f9b6811a615c45a9504f20
Peter Pivarnik <peter.pivarnik@gmail.com>
2025-04-02 09:14:29 +0200

    Initial commit

---
+++ src/main/kotlin/com/prewave/edge/EdgeApplication.kt
@@ -0,0 +1,11 @@
+package com.prewave.edge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdgeApplication>(*args)
+}
---
+++ src/test/kotlin/com/prewave/edge/EdgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.prewave.edge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
